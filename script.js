798177f5902ed1da1fb7b8c066de3d12c84ee678
unknown <RNB-CLUB>
2025-12-06 17:46:48 +0200

    v53

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ let main = document.querySelector("main")
 let products = []
 
 
-function getProducts(){
+function getProducts() {
     fetch(BASE_URL + "/products")
-    .then(async(res)=>{
-        let data = await res.json()
-        console.log(data)
-        products = data
-        drawProducts()
-    })
+        .then(async (res) => {
+            let data = await res.json()
+            console.log(data)
+            products = data
+            drawProducts()
+        })
 }
-function drawProducts(){
+function drawProducts() {
     main.innerHTML = ""
-    products.forEach(p=>{
+    products.forEach(p => {
         main.innerHTML += `
         <div class="product">
         <h3>${p.name}</h3>
-        <a href="/seller/${p.seller_id}">SELLER PAGE</a>
+        <a href="/seller.html?user_id=${p.seller_id}">SELLER PAGE</a>
         <p>${p.category}</p>
         <p>cost - ${p.price}</p>
         <p>${p.in_stock}</p> 
---
+++ seller.js
@@ -0,0 +1,46 @@
+const urlParams = new URLSearchParams(window.location.search)
+let user_id = urlParams.get("user_id")
+console.log(user_id)
+
+const BASE_URL = "https://my-json-server.typicode.com/RNB-CLUB/Store"
+let main = document.querySelector("main")
+
+function getUsers() {
+    fetch(BASE_URL + "/users?user_id=" + user_id)
+        .then(async (res) => {
+            let data = await res.json()
+            data = data[0]
+            document.querySelector(".seller h1").innerHTML = data.username
+            document.querySelector(".seller h3").innerHTML = "Пошта:" + data.email
+            document.querySelector(".seller h4").innerHTML = "#" + data.role
+            console.log(data)
+        })
+}
+function getProducts() {
+    fetch(BASE_URL + "/products?user_id" + user_id)
+        .then(async (res) => {
+            let data = await res.json()
+            console.log(data)
+            drawProducts(data)
+        })
+}
+function drawProducts(products) {
+    main.innerHTML = ""
+    products.forEach(p => {
+        main.innerHTML += `
+        <div class="product">
+        <h3>${p.name}</h3>
+        <p>${p.category}</p>
+        <p>cost - ${p.price}</p>
+        <p>${p.in_stock}</p> 
+        </div> `
+    })
+}
+getProducts()
+getUsers()
+
+let cartButton = document.getElementById("cart")
+let cartIsOpen = false
+cartButton.addEventListener("click", function(){
+    cartIsOpen = !cartIsOpen
+})
